2c9df16ba9e2942c76cc2397a94690883bf41403
Bkdpchaturvedi-Mac <bkdpchaturvedi@gmail.com>
2018-06-02 12:19:02 +0800

    initial Code base

---
+++ src/Pages/Member/upload/S3UploadFunction.js
@@ -0,0 +1,22 @@
+import { Storage } from "aws-amplify";
+
+export async function s3Upload(file,userid) {
+  const filename = `${userid}-${Date.now()}-${file.name}`;
+
+  const stored = await Storage.vault.put(filename, file, {
+    contentType: file.type
+  });
+
+  return stored.key;
+}
+
+export async function s3UploadPublic(file,userid) {
+  const filename = `${userid}-${Date.now()}-${file.name}`;
+
+  const stored = await Storage.put(filename, file, {
+    contentType: file.type,
+    level:"public"
+  });
+
+  return stored.key;
+}
